15b2d124c2ddb4b22ee962e844e209177205af86
Trunin Oleh <trunin.o@hexide-digital.com>
2024-11-03 18:32:21 +0200

    feat: install sqlServer and tools

--- BookArchive/Controllers/AuthController.cs
+++ BookArchive/Controllers/AuthController.cs
@@ -1,113 +1,113 @@
-using System.Security.Claims;
-using book_archive.Models;
-using book_archive.ViewModel;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
-namespace BookArchive.Controllers
-{
-    public class AuthController : Controller
-    { 
-        private readonly DbContext _context;
-
-        public AuthController(DbContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        public string AccessDenied()
-        {
-            return "Недостатньо прав";
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                User user = await _context.Users
-                    .Include(u => u.Role)
-                    .FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
-                if (user != null)
-                {
-                    await Authenticate(user); // аутентификация
-                    return RedirectToAction("Index", "Home");
-                }
-
-                ModelState.AddModelError("", "Неправильні логін чи пароль");
-            }
-
-            return View(model);
-        }
-
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(RegisterModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                User user = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
-                if (user == null)
-                {
-                    user = new User { Email = model.Email, Password = model.Password };
-                    Role userRole = await _context.Roles.FirstOrDefaultAsync(r => r.Name == "user");
-                    if (userRole != null)
-                        user.Role = userRole;
-                    _context.Users.Add(user);
-                    await _context.SaveChangesAsync();
-                    await Authenticate(user);
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                    ModelState.AddModelError("", "Неправильні логін чи пароль");
-            }
-
-            return View(model);
-        }
-
-        private async Task Authenticate(User user)
-        {
-            if (user == null)
-            {
-                throw new ArgumentNullException(nameof(user));
-            }
-
-            var claims = new List<Claim>();
-            if (!string.IsNullOrEmpty(user.Email))
-            {
-                claims.Add(new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email));
-            }
-
-            if (!string.IsNullOrEmpty(user.Role?.Name))
-            {
-                claims.Add(new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role.Name));
-            }
-
-            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
-                ClaimsIdentity.DefaultRoleClaimType);
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
-                new ClaimsPrincipal(id));
-        }
-
-        public async Task<IActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Login", "Account");
-        }
-    }
-}
+// using System.Security.Claims;
+// using book_archive.Models;
+// using book_archive.ViewModel;
+// using Microsoft.AspNetCore.Authentication;
+// using Microsoft.AspNetCore.Authentication.Cookies;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+//
+// namespace BookArchive.Controllers
+// {
+//     public class AuthController : Controller
+//     { 
+//         private readonly DbContext _context;
+//
+//         public AuthController(DbContext context)
+//         {
+//             _context = context;
+//         }
+//
+//         [HttpGet]
+//         public IActionResult Login()
+//         {
+//             return View();
+//         }
+//
+//         public string AccessDenied()
+//         {
+//             return "Недостатньо прав";
+//         }
+//
+//         [HttpPost]
+//         [ValidateAntiForgeryToken]
+//         public async Task<IActionResult> Login(LoginModel model)
+//         {
+//             if (ModelState.IsValid)
+//             {
+//                 // User user = await _context.Users
+//                 //     .Include(u => u.Role)
+//                 //     .FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+//                 // if (user != null)
+//                 // {
+//                 //     await Authenticate(user); // аутентификация
+//                 //     return RedirectToAction("Index", "Home");
+//                 // }
+//
+//                 ModelState.AddModelError("", "Неправильні логін чи пароль");
+//             }
+//
+//             return View(model);
+//         }
+//
+//         [HttpGet]
+//         public IActionResult Register()
+//         {
+//             return View();
+//         }
+//
+//         [HttpPost]
+//         [ValidateAntiForgeryToken]
+//         public async Task<IActionResult> Register(RegisterModel model)
+//         {
+//             if (ModelState.IsValid)
+//             {
+//                 // User user = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+//                 // if (user == null)
+//                 // {
+//                     // user = new User { Email = model.Email, Password = model.Password };
+//                     // Role userRole = await _context.Roles.FirstOrDefaultAsync(r => r.Name == "user");
+//                     // if (userRole != null)
+//                     //     user.Role = userRole;
+//                     // _context.Users.Add(user);
+//                     // await _context.SaveChangesAsync();
+//                     // await Authenticate(user);
+//                     return RedirectToAction("Index", "Home");
+//                 // }
+//                 // else
+//                     ModelState.AddModelError("", "Неправильні логін чи пароль");
+//             }
+//
+//             return View(model);
+//         }
+//
+//         private async Task Authenticate(User user)
+//         {
+//             if (user == null)
+//             {
+//                 throw new ArgumentNullException(nameof(user));
+//             }
+//
+//             var claims = new List<Claim>();
+//             if (!string.IsNullOrEmpty(user.Email))
+//             {
+//                 claims.Add(new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email));
+//             }
+//
+//             if (!string.IsNullOrEmpty(user.Role?.Name))
+//             {
+//                 claims.Add(new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role.Name));
+//             }
+//
+//             ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
+//                 ClaimsIdentity.DefaultRoleClaimType);
+//             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+//                 new ClaimsPrincipal(id));
+//         }
+//
+//         public async Task<IActionResult> Logout()
+//         {
+//             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+//             return RedirectToAction("Login", "Account");
+//         }
+//     }
+// }
