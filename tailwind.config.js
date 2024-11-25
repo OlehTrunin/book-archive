ca393ea7ed10a70696cb813db740f230d51c6ffc
Trunin Oleh <trunin.o@hexide-digital.com>
2024-11-25 22:07:39 +0200

    feat: restyle with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './BookArchive/Pages/**/*.cshtml',
+    './BookArchive/Views/**/*.cshtml'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
